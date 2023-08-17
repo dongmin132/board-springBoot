754df659ab4e0bd773575ff6b5bee30b12f4bbed
dongmin <m914@naver.com>
2023-08-17 21:23:02 +0900

    #12 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭 되도록 설계
    생성자와 '`equals()`, `hashcode()` 등이 도메인 맥락에 맞게끔 오버 라이딩

--- board-project/src/main/java/com/min/board/domain/Article.java
+++ board-project/src/main/java/com/min/board/domain/Article.java
@@ -1,15 +1,82 @@
 package com.min.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;       //제목
-    private String content;     //내용
-    private String hashtag;     //해시태그로 검색하기
 
-    private LocalDateTime createAt;
+    @Setter @Column(nullable = false)
+    private String title;       //제목
+    @Setter @Column(nullable = false,length = 10000)
+    private String content;     //내용
+    @Setter private String hashtag;     //해시태그로 검색하기
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @CreatedBy
     private String createdBy;
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
     private String modifiedBy;
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();      //중복을 허용하지 않고 모아서 컬렉션으로 보겠다는 의도의 Set<>
+
+
+    protected Article() {
+
+    }
+
+    private Article(String title, String content, String hashtag) {         //접근 제어자를 private으로 둠으로써 팩토리 메서드로 받아서 사용하게 설정
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {       //동일성 검사를 실행함.
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
+
