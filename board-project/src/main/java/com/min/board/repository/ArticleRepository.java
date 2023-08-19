da76c37931deb18899bcad522a189b52d5b66949
dongmin <m914@naver.com>
2023-08-19 17:16:39 +0900

    #1-Data Rest 테스트

--- board-project/src/main/java/com/min/board/repository/ArticleRepository.java
+++ board-project/src/main/java/com/min/board/repository/ArticleRepository.java
@@ -3,7 +3,9 @@ package com.min.board.repository;
 import com.min.board.domain.Article;
 import com.min.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
