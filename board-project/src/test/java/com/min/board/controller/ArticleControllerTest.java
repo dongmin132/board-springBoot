bd60566751d232e0f5be305b41c9ed2eca8bcf95
dongmin <m914@naver.com>
2023-08-20 16:16:04 +0900

    #18 - 게시글 관련 뷰 테스트 내용 업데이트
    추가로 뷰에 이름을 확인하는 테스트 기능을 추가하였다.

--- board-project/src/test/java/com/min/board/controller/ArticleControllerTest.java
+++ board-project/src/test/java/com/min/board/controller/ArticleControllerTest.java
@@ -12,8 +12,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static io.micrometer.core.instrument.binder.http.HttpRequestTags.status;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(ArticleController.class)        //입력한 컨트롤러만 체크해서 테스트할 수 있다.
@@ -31,6 +30,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))   // 실제 view에 이름을 검색한다.
                 .andExpect(model().attributeExists("articles"));    //"articles"라는 키 값이 있는지 확인한다.
 
     }
@@ -42,7 +42,9 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(model().attributeExists("article"));    //"article"라는 키 값이 있는지 확인한다.
+                .andExpect(view().name("articles/detail"))   // 실제 view에 이름을 검색한다.
+                .andExpect(model().attributeExists("article"))    //"article"라는 키 값이 있는지 확인한다.
+                .andExpect(model().attributeExists("articleComments"));    //article에는 댓글들도 포함되어있다.
 
     }
 
@@ -53,7 +55,9 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));   // 실제 view에 이름을 검색한다.
+
 
     }
     @DisplayName("[view] [GET] 게시글 해시태그 검색 전용 페이지 - 정상 호출")
@@ -62,6 +66,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));   // 실제 view에 이름을 검색한다.
     }
 }
